63b8fb8380a09b40e0501b7046fca789eb406894
Abhinav Sharma <ahvnav01@gmail.com>
2026-05-27 21:26:04 +0530

    Update S3 bucket name in backend configuration

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "terraform-statefile-host-s3-bucket"
+        bucket = "statefile-host-bucket"
         key = "vpc_peering/terraform.tfstate"
         region = "ap-south-1"
         encrypt = true
